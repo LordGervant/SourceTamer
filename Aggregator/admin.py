9f17278e2743310311d6d88c6559a00a707e55e9
Максим Пономарев <alpha18022@gmail.com>
2019-08-26 10:47:05 +0500

    Added Models Commit

--- Aggregator/admin.py
+++ Aggregator/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from ContentAggregator.Aggregator.models import article, feed
 # Register your models here.
+
+@admin.register(article.Article)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in article.Article._meta.fields]
+
+@admin.register(feed.Feed)
+class FeedAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in feed.Feed._meta.fields]
